7d936d4dc142494cb6b607d69040bbb3293daa9f
Hazel Shih <hazelshih17@gmail.com>
2021-04-25 13:21:03 +0800

    finish_week2_HW

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,22 @@
 function join(arr, concatStr) {
-  
+  let ans = "";
+  for (let i = 0; i < arr.length - 1; i++) {
+    let temp = "";
+    temp = arr[i] + concatStr;
+    ans += temp;
+  }
+  ans += arr[arr.length - 1].toString();
+  return ans;
 }
+
 
 function repeat(str, times) {
-  
+  let ans = "";
+  for(let i = 1; i <= times; i++){
+    ans += str;
+  }
+  return ans;
 }
 
-console.log(join(['a'], '!'));
+console.log(join(["a"], ','));
 console.log(repeat('a', 5));
