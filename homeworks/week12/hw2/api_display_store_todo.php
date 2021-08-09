2a9e5fb215bdff98d6e8bad3d11312be8cbe976e
Hazel Shih <hazelshih17@gmail.com>
2021-08-09 16:51:22 +0800

    Complete week12 hw1 & hw2

---
+++ homeworks/week12/hw2/api_display_store_todo.php
@@ -0,0 +1,44 @@
+<?php 
+  require_once('conn.php');
+  header('Access-Control-Allow-Origin: *');
+  header('Content-type:application/json;chartset=utf8');
+
+  if(empty($_GET['todosID'])){
+    $arr = array(
+      'ok' => false,
+      "message" => '沒有 todosID'
+    );
+    $json = json_encode($arr);
+    echo $json;
+    die();
+  }
+
+  $todosID = $_GET['todosID'];
+  
+  $sql = 'SELECT * FROM hazel_w12_todoList WHERE todos_id = ?';
+  $stmt = $conn -> prepare($sql);
+  $stmt -> bind_param('s', $todosID);
+  $result = $stmt -> execute();
+
+  if(!$result){
+    $arr = array(
+      "ok" => false,
+      "message" => "Oops! There's an error here."
+    );
+    $json = json_encode($arr);
+    echo $json;
+    die();
+  }
+
+  $result = $stmt -> get_result();
+  $todos = $result -> fetch_assoc();
+
+  $arr = array(
+    "ok" => true,
+    "message" => "success!",
+    "todos" => $todos
+  );
+  
+  $json = json_encode($arr);
+  echo $json;
+?>
